d4ed1e7364c1694649a6166a7311e236a8247795
Pablo Aller <pablusprogramador@gmail.com>
2022-04-13 20:16:08 +0200

    css

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const { Server } = require("socket.io")
-const io = new Server(server)
+//const { Server } = require("socket.io")
+//const io = new Server(server)
 
 app.get('/', (req, res) => {
 
     res.status(200).sendFile(__dirname + '/public/index.html');
 })
 
-io.on('connection', (socket) => {
-    console.log('un usuario se ha conectado')
-})
+// io.on('connection', (socket) => {
+//     console.log('un usuario se ha conectado')
+// })
 
 app.listen(port, () => {
     console.log(`Server corriendo en el puerto: *${port}*`)
