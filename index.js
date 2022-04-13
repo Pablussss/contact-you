7f65f81de4ae3a039247ca97aa7742a769872b70
Pablo Aller <pablusprogramador@gmail.com>
2022-04-13 20:46:40 +0200

    setup server socket

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const http = require('http');
-const server = http.createServer(app)
-const { Server } = require("socket.io")
-const io = new Server(server)
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
+// Estatico
 app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
-    res.status(200).sendFile(__dirname + '/public/index.html');
-})
-
+// Chat
 io.on('connection', (socket) => {
-    console.log('un usuario se ha conectado')
-})
+    console.log('a user connected');
+});
 
-app.listen(port, () => {
+// Server start
+server.listen(port, () => {
     console.log(`Server corriendo en el puerto: *${port}*`)
 })
