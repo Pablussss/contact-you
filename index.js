497a20aa8f040d111d773f7642d482e4aeb55433
Pablo Aller <pablusprogramador@gmail.com>
2022-04-13 19:25:37 +0200

    continuacion

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
 const app = express();
-
 const port = 3000;
 
 app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send(console.log("Hola"))
+})
 
 app.listen(port, () => {
     console.log(`Server corriendo en port ${port}`)
