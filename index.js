73c5f8de08528155ab5acc79aa04136f0f53b0b0
Pablo Aller <pablusprogramador@gmail.com>
2022-04-13 19:57:03 +0200

    setup server

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'))
-
 app.get('/', (req, res) => {
-    res.send(console.log("Hola"))
+
+    res.status(200).sendFile(__dirname + '/public/index.html');
 })
 
 app.listen(port, () => {
