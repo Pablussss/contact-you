0c568173e4cc10c69734b3b04b8eb93422084385
Pablo Aller <pablusprogramador@gmail.com>
2022-04-18 20:01:28 +0200

    continuacion

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const http = require('http');
-const server = http.createServer(app);
+const server = require('http').createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
