254205702fd46b8de501cbc3b7e52c481e08ce5c
Pablo Aller <pablusprogramador@gmail.com>
2022-04-19 14:34:28 +0200

    usuarios funcionando

--- socket.js
+++ socket.js
@@ -1,13 +1,42 @@
 module.exports = function (io) {
 
-    let nicknames = ['guada', 'didrick'];
+    let usernames = [' '];
 
     io.on('connection', socket => {
-        console.log('new user connected')
-
+        
         // escuchar mensajes servidor y emitirlos a los clientes
         socket.on('send message', data => {
-            io.emit('new message', data)
+            io.emit('new message', {
+                msg: data,
+                user: socket.username
+            })
         })
+
+        // conexion nuevo usuario 
+        socket.on('new user', (data, res) => {
+            console.log(data + " se ha conectado")
+            if(usernames.indexOf(data) !== -1) {
+                res(false)
+            } else {
+                res(true);
+                socket.username = data;
+                usernames.push(socket.username);
+                updateUsers()
+            }
+        })
+        // desconexion usuario
+        socket.on('disconnect', data => {
+            if (!socket.username) return
+            let desconectado = socket.username;
+            console.log(desconectado + " se ha desconectado")
+
+            usernames.splice(desconectado, 1)
+            updateUsers()
+        })
+
+        // Actualizar lista usuarios
+        function updateUsers () {
+            io.emit('usernames', usernames)
+        }    
     })
 }
